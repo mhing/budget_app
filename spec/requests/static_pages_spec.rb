04bfd654be54fbba2240c6a9b8fbd72af4d24b91
Matt Hinger <matthew.hinger@gmail.com>
2014-01-29 22:34:37 -0500

    Refactor test files

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,7 +8,7 @@ describe "Static pages" do
   	before { visit root_path }
 
 	it { should have_content('Budget Planner') }
-    it { should have_title("Budget Planner") }
+    it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
 
   end
@@ -17,8 +17,19 @@ describe "Static pages" do
   	before { visit about_path }
 
 	it { should have_content('About Budget Planner') }
-    it { should have_title("Budget Planner | About") }
+    it { should have_title(full_title('About')) }
 
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About'))
+    click_link "Budget Planner"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "Budget Planner"
+    expect(page).to have_title(full_title(''))
   end
 
 end
