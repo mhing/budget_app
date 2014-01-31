ea8489c2c269ba0718cdbbd220acbe562a097b14
Matt Hinger <matthew.hinger@gmail.com>
2014-01-30 21:22:27 -0500

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 BudgetApp::Application.routes.draw do
-  #get "users/new"
-
+  resources :users
   root 'static_pages#home'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/signup', to: 'users#new', via: 'get'
