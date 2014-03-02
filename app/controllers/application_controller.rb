9569e300f2ed2579e510a7d826d49643b839c1be
Matt Hinger <matthew.hinger@gmail.com>
2014-03-02 17:46:25 -0500

    Adding redirect from budgetplanner.herokuapp.com

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  include SessionsHelper
+	before_filter :redirect_if_old
+
+
+	# Prevent CSRF attacks by raising an exception.
+	# For APIs, you may want to use :null_session instead.
+	protect_from_forgery with: :exception
+	include SessionsHelper
+
+	protected
+
+		def redirect_if_old
+			if request.host == 'budgetplanner.herokuapp.com'
+				redirect_to "http://budget-it.herokuapp.com#{request.request_uri}", :status => :moved_permanently
+			end
+		end
 end
