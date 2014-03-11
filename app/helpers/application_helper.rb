dad33cd96ef885990bf9484859a1a7fae9fbe514
Matt Hinger <matthew.hinger@gmail.com>
2014-03-10 22:48:58 -0400

    Add a display for net worth based on submitted incomes and expense totals

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,39 @@ module ApplicationHelper
 	def new_expense_page?
 		request.fullpath == new_expense_path
 	end
+
+	def expenses_total
+		sum = 0
+		user = current_user
+		budget = user.budget
+		if !budget.nil?
+			expenses = budget.expenses
+			if !expenses.blank?
+				expenses.each do |exp|
+					sum += exp.amount
+				end
+			end
+		end
+		sum = -sum
+	end
+
+	def incomes_total
+		sum = 0
+		user = current_user
+		budget = user.budget
+		if !budget.nil?
+			incomes = budget.incomes
+			if !incomes.blank?
+				incomes.each do |inc|
+					sum += inc.amount
+				end
+			end
+		end
+		sum
+	end
+
+	def net_worth
+		net = incomes_total + expenses_total
+	end
+
 end
